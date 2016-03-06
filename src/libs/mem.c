957742bb94ac2415bc9ff3828f04d180e4139e65
Cody Creager <pi.rubiks@gmail.com>
2016-03-06 10:48:37 -0800

    An example plugin, just gets a random int right now.

--- src/libs/mem.c
+++ src/libs/mem.c
@@ -5,4 +5,15 @@
  *      Author: cody
  */
 
+#include "../ttysys_api.h"
+#include <stdio.h>
 
+int nextVal(float* outs)
+{
+	float out = 0;
+	FILE* fp = fopen("/dev/urandom", "r");
+	out = (float)fgetc(fp);
+	fclose(fp);
+	outs[0] = out*100.0/255;
+	return 0;
+}
