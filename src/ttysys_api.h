c60642f1db0c7b6149d6884e056ac2da861180fc
Cody Creager <pi.rubiks@gmail.com>
2016-04-03 20:38:25 -0700

    basic api function definitions with init, destroy, etc

--- src/ttysys_api.h
+++ src/ttysys_api.h
@@ -8,6 +8,27 @@
 #ifndef SRC_TTYSYS_API_H_
 #define SRC_TTYSYS_API_H_
 
-typedef int (nextValueFunc)(float* vals);
+#define TITLE_LEN 100
+
+enum winType { VoidChart, PercentChart, ValueChart, TextChart };
+
+struct initData;
+
+// You should have a function called 'init' that matches this signature:
+typedef struct initData (initFunc)(void** context, int argc, char** argv);
+struct initData init(void** context, int argc, char** argv);
+
+typedef int (nextValueFunc)(void** context, float* vals);
+typedef int (titleFunc)(void** context, char title[TITLE_LEN]);
+typedef void (cleanupFunc)(void** context);
+
+// When a plugin returns this, NULL means ttysys won't call the function.
+struct initData
+{
+	nextValueFunc* nextValue;
+	cleanupFunc* cleanUp;
+	titleFunc* title;
+	enum winType type;
+};
 
 #endif /* SRC_TTYSYS_API_H_ */
