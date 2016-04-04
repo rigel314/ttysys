c2a2585d1665ad7024c149e4149e450e6c38985e
Cody Creager <pi.rubiks@gmail.com>
2016-04-03 21:48:38 -0700

    Added memory plugin using the orignal memory code.

--- src/libs/mem.h
+++ src/libs/mem.h
@@ -8,6 +8,27 @@
 #ifndef MEM_H_
 #define MEM_H_
 
+// Holds numbers from /proc/meminfo in kiB.
+struct memNow
+{
+	long ramTotal;
+	long ramFree;
+	long ramBuffered;
+	long ramCached;
+	long swapTotal;
+	long swapFree;
+};
 
+// Holds percentages for RAM and Swap.
+struct memPercent
+{
+	struct memNow now;
+	float ram;
+	float swap;
+};
+
+long parseMemLine(char* str, int len);
+struct memNow readMem();
+void getMemInfo(struct memPercent* mem);
 
 #endif /* MEM_H_ */
